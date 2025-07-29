aa36fe90694eb3c550facd94fc064a08f030d741
hen2004 <Henratsabi@gmail.com>
2025-07-29 18:11:22 +0300

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '''
+        <html>
+            <head><title>Hello Page</title></head>
+            <body style="text-align: center; margin-top: 100px;">
+                <h1 style="font-size: 72px;">HELLO WORLD</h1>
+            </body>
+        </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
